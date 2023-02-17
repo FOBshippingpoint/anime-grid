75305262ca4e38154e85dba171d81d801609b7d5
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2023-02-17 17:00:21 +0800

    feat: add multilang support

---
+++ html/translate.js
@@ -0,0 +1,25 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/language
+const availableLangs = ['zh-TW', 'zh', 'zh-CN', 'en', 'ja'];
+
+async function loadLocale() {
+  try {
+    let userLang = navigator.language || navigator.userLanguage;
+    if (availableLangs.indexOf(userLang) === -1) {
+      userLang = "en";
+    }
+    userLang = "ja";
+    const response = await fetch(`./_locale/${userLang}.json`);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function createTranslate(locale) {
+  return (key) => {
+    return locale[key] || key;
+  };
+}
+
+export { createTranslate, loadLocale };
